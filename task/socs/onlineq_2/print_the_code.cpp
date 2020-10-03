a7d44351fdb13387a367636170737c2935265e47
Mr.Miss <keselekpermen69@gmail.com>
2020-10-03 13:32:59 +0700

    fix
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>

--- task/socs/onlineq_2/print_the_code.cpp
+++ task/socs/onlineq_2/print_the_code.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main () {
-	char str[101];
-	scanf("%[^\n]", str);
+	char S[101];
+	scanf("%[^\n]", S);
 	printf("#include <stdio.h>\n");
 	printf("int main()\n");
 	printf("{\n");
-	printf("    printf(\"%%s\\n\", \"%s\");\n", str);
+	printf("    printf(\"\%%s\\n\",\"\%s\");\n", S);
 	printf("    return 0;");
 	printf("\n}\n");
 	return 0;
